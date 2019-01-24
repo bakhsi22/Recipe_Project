51cab4246e1134a5268b23bb7bfd9dda83ae60f2
bakshi22 <bak.shobhit@gmail.com>
2019-01-24 16:47:38 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Form from "./components/Form";
 class App extends Component {
   render() {
     return (
@@ -20,6 +19,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Form />
       </div>
     );
   }
