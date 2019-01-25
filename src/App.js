b47f06e9a4fa394cb13d4bcf3ee715349c185169
bakshi22 <bak.shobhit@gmail.com>
2019-01-25 12:40:37 +0530

    get array

--- src/App.js
+++ src/App.js
@@ -2,25 +2,48 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Form from "./components/Form";
+  // key
+  // https://www.food2fork.com/api/search?key=9e733f71085841778fbab780de76000c&q=shredded%20chicken
+const API_KEY="9e733f71085841778fbab780de76000c";
 class App extends Component {
+  state={
+    //setting an array to store api returned 'data'
+    recipes:[]
+  }
+  // we can declare state w/o constructor function  
+  getRecipe =async(e) => {
+    // const recipeName= e.target.elements.recipeName.value;
+    e.preventDefault();
+
+    // using async await function to make a call  to the API
+
+    const api_call=await fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=10`) 
+    // console.log(recipeName)
+    //to grab data creating new const
+    //using await to fetch the API and convert into json
+    const data = await api_call.json();
+    //as website dont allow local servers to access their data
+    //we disable cors :
+    //turn off Cross Origin Restrictions in Chrome is to turn off 
+    // the feature completely when starting the browser by passing arguments to it.
+    // e.g. --disable-web-security 
+    // --user-data-dir=~/.chrome-disable-web-security
+    // or use cors anywhere that makes it appear to be a live server
+    // by including "https://cors-anywhere.herokuapp.com/" before link fetch 
+    
+    this.setState({recipes:data.recipes}) 
+    console.log(this.state.recipes);
+
+  
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Recipe Search</h1>
         </header>
-        <Form />
+        {/* Passing data to another component using props  */}
+        <Form getRecipe={this.getRecipe}/>
       </div>
     );
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
 const Form = props => (
+  
+// onSubmit is a built in react attribute   
+
   <form onSubmit={props.getRecipe}>
-    <input type="text"  />
+
+    <input type="text"  name="recipeName"/>
+{/* 
+// we cant use this keyword becoz it works only in class based
+// components
+//here we have a stateless component so we have to pass an 
+//argument 
+// props => */}
+
     <button > Search </button>
   </form>
 );
