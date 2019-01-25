7066004460197ea2f4d2603072fd67c13a703901
bakshi22 <bak.shobhit@gmail.com>
2019-01-25 14:03:12 +0530

    keyfixed_displayed_ImagesAnd SearchItems

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Form from "./components/Form";
-  // key
-  // https://www.food2fork.com/api/search?key=9e733f71085841778fbab780de76000c&q=shredded%20chicken
-const API_KEY="9e733f71085841778fbab780de76000c";
-class App extends Component {
-  state={
+    // key
+    // https://www.food2fork.com/api/search?key=9e733f71085841778fbab780de76000c&q=shredded%20chicken&count=5
+    const API_KEY="9e733f71085841778fbab780de76000c";
+    class App extends Component {
+    state={
     //setting an array to store api returned 'data'
     recipes:[]
   }
-  // we can declare state w/o constructor function  
-  getRecipe =async(e) => {
-    // const recipeName= e.target.elements.recipeName.value;
+    // we can declare state w/o constructor function  
+    getRecipe =async(e) => {
+    const recipeName= e.target.elements.recipeName.value;
     e.preventDefault();
 
     // using async await function to make a call  to the API
 
-    const api_call=await fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=10`) 
+    const api_call=await 
+    fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=5`) 
     // console.log(recipeName)
     //to grab data creating new const
     //using await to fetch the API and convert into json
@@ -45,7 +46,13 @@ class App extends Component {
         {/* Passing data to another component using props  */}
         <Form getRecipe={this.getRecipe}/>
         {this.state.recipes.map((recipe)=>{
-          return  <p key ={recipe.recipe_id}>{ recipe.title }</p>
+          return(
+          <div key ={recipe.recipe_id}>
+            <img src={recipe.image_url} alt={recipe.title}/>
+            <p >{ recipe.title }</p>
+          </div> 
+          // return  <p key ={recipe.recipe_id}>{ recipe.title }</p>
+          );
         })}
       </div>
     );
