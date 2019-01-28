ea0beb349a6292e9875b5d158e051d017866152d
bakshi22 <bak.shobhit@gmail.com>
2019-01-28 15:45:28 +0530

    starting to style

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -4,36 +4,22 @@ import React from 'react';
 
 const Recipes = props => (
     //implicity returning js
-    <div>
-        {props.recipes && props.recipes.map((recipe)=>{
-        return(
-        //providing the key in the most parent div
-        <div key ={recipe.recipe_id}>
-            <img src={recipe.image_url} alt={recipe.title}/>
-            <p >{ recipe.title }</p>
-        </div> 
+    <div className="container">
+        <div className="row">
+            {props.recipes && props.recipes.map((recipe)=>{
+            return(
+            //providing the key in the most parent div
+            <div key ={recipe.recipe_id}>
+                <img src={recipe.image_url} alt={recipe.title}/>
+                <p >{ recipe.title }</p>
+            </div> 
         // return  <p key ={recipe.recipe_id}>{ recipe.title }</p>
         );
         }) }
         {/* Recipes ! */}
+        </div>
     </div>  
 );
 
 export default Recipes ;
  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
