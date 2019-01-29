197dc9b4366a9c5180ce039e1017d0aed9c32482
bakshi22 <bak.shobhit@gmail.com>
2019-01-29 15:16:36 +0530

    jan29_styling

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -6,19 +6,30 @@ const Recipes = props => (
     //implicity returning js
     <div className="container">
         <div className="row">
-            {props.recipes && props.recipes.map((recipe)=>{
-            return(
-            //providing the key in the most parent div
-            <div key ={recipe.recipe_id}>
-                <img src={recipe.image_url} alt={recipe.title}/>
-                <p >{ recipe.title }</p>
-            </div> 
-        // return  <p key ={recipe.recipe_id}>{ recipe.title }</p>
-        );
-        }) }
-        {/* Recipes ! */}
-        </div>
-    </div>  
+            {props.recipes.map((recipe) => {
+                return (
+                    <div key={recipe.title}  className="col-md-4" 
+                    style={{marginBottom:"2rem"}} >
+                        <div className="recipes__box">
+                            <img 
+                                className="recipe__box-img" 
+                                src={recipe.image_url}
+                                alt={recipe.title} />
+                                <div className="recipe__text">
+                                    <h5 className="recipes__title" >{recipe.title}</h5>
+                                    <p className="recipes__subtitle">Publisher: <span>
+                                        {recipe.publisher}
+                                        </span> </p>
+                                </div>
+                                <button className="recipe_buttons">View Recipe</button>
+                        </div>
+                    </div>
+                );
+            })
+
+            }
+        </div>        
+    </div>
 );
 
 export default Recipes ;
